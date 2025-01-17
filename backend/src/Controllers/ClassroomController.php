e646aae58a8d27accc6bfb0fec47d5aaaf299891
Szymon Górnik <gornikszymon8@gmail.com>
2025-01-17 21:03:38 +0100

    Szymon zmiany w ClassroomController

--- backend/src/Controllers/ClassroomController.php
+++ backend/src/Controllers/ClassroomController.php
@@ -2,30 +2,39 @@
 namespace App\Controllers;
 
 use App\Controller;
+use App\Database as AppDatabase;
 
 class ClassroomController extends Controller
 {
     public function getClassroom()
     {
         $room = $_GET['room'] ?? null;
+        $building = $_GET['building'] ?? null;
 
         if (!$room) {
             $this->jsonResponse(['error' => 'Room is required'], 400);
+            return;
         }
 
-        $apiUrl = "https://plan.zut.edu.pl/schedule.php?kind=room&query=" . urlencode($room);
-
         try {
-            $data = $this->httpGet($apiUrl);
-            if ($data === false) {
-                throw new \Exception('Failed to fetch data');
+            $db  = AppDatabase::getConnection();
+            if ($building) {
+                
+            $query = $db->prepare("SELECT item FROM classrooms WHERE item LIKE :room AND building LIKE :building");
+            $query->bindValue(':room', '%' . $room . '%', \PDO::PARAM_STR);
+            $query->bindValue(':building', '%' . $building . '%', \PDO::PARAM_STR);
             }
-            $this->jsonResponse(json_decode($data, true));
+            else {
+                $query = $db->prepare("SELECT item FROM classrooms WHERE item LIKE :room");
+                $query->bindValue(':room', '%' . $room . '%', \PDO::PARAM_STR); 
+            }
+
+            $query->execute();
+            $result = $query->fetchAll(\PDO::FETCH_ASSOC);
+            $this->jsonResponse($result);
+
         } catch (\Exception $e) {
             $this->jsonResponse(['error' => 'Unable to fetch classroom schedule', 'details' => $e->getMessage()], 500);
         }
     }
 }
-
-
-
